9e3e1b217fa286b57215bc8d8bf8bd5d9e5d18bd
yiyusheng-crane23 <yiyusheng.hust@gmail.com>
2018-09-20 16:36:01 +0800

    0920:copy code

--- m/gen_month_lastid1.m
+++
@@ -1,5 +0,0 @@
-run generate_data_model1.m
-format long g
-mon_lastid_train = month_lastid(smart1_train)
-mon_lastid_test = month_lastid(smart1_test)
-mon_lastid_all = month_lastid(smart1_all)
--- m/gen_month_lastid2.m
+++
@@ -1,5 +0,0 @@
-run generate_data_model2.m
-format long g
-mon_lastid_train = month_lastid(smart2_train)
-mon_lastid_test = month_lastid(smart2_test)
-mon_lastid_all = month_lastid(smart2_all)
